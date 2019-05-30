abfd8528908f712cf7442d1881ede96cac59aa4a
caoshiyuan <codesircao@126.com>
2019-05-30 23:05:59 +0800

    add diff string

---
+++ vue/learning/src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <selectTree></selectTree>
+  </div>
+</template>
+
+<script>
+import selectTree from "./select.vue";
+export default {
+  name: "index",
+  data() {
+    return {
+      msg: "index"
+    };
+  },
+  components: {
+    selectTree
+  }
+};
+</script>
+
+</style>
