61c369e0eb076b233a4059aba33454559ce64dca
caoshiyuan <caoshiyuan@emicnet.com>
2019-08-20 16:25:38 +0800

    a

--- git/webhookNode/routes/users.js
+++ git/webhookNode/routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function (ctx, next) {
 })
 
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+  ctx.body = 'this 1is a users/bar response'
 })
 
 module.exports = router
