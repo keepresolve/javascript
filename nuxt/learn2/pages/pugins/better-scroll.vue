06f67d3ab443d465e58b884cb68c4325ed075065
keepresolve <164840613@qq.com>
2019-12-05 00:17:20 +0800

    better-scroll

--- nuxt/learn2/pages/pugins/better-scroll.vue
+++ nuxt/learn2/pages/pugins/better-scroll.vue
@@ -6,17 +6,36 @@
         </header>
         <div class='container' ref='container'>
             <ul class='wrapper'>
-                <li v-for="(item,index) in lis" :ref='`target_${item.key}`' :key='index'>{{item.key}}</li>
+                <li v-for="item in lis" :ref='`target_${item.index}`' :key='item.index'>
+                    <h3>{{item.index||item.title}}</h3>
+                    <ul>
+                        <li v-for="suItem in item.list" :key="suItem.key">
+                            {{suItem.value}}
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
-        <div class='bar' @touchmove="tap">
+        <div class='bar' @touchstart='indexStart'>
             <ul>
-                <li v-for="(item,index) in barList" :data-key='item.key' :key='index' @click="scrollto(item)">{{item.key}}</li>
+                <li class='indexlist-navitem' v-for="(item,index) in indexs" :data-key='item.key' :key='index' @click="scrollto(item)">
+
+                    {{item.index}}
+                    <span class="indexlist-title" :class="{hidden:item.hidden}">{{item.key}}</span>
+                </li>
             </ul>
         </div>
     </div>
 </template>
 <script>
+const getAllChars = () => {
+    let startChar = 65;
+    const allChars = [];
+    while (startChar < 65 + 26) {
+        allChars.push(String.fromCharCode(startChar++));
+    }
+    return allChars;
+};
 import BScroll from "better-scroll";
 export default {
     name: "better-scroll",
@@ -27,15 +46,23 @@ export default {
     },
     data() {
         return {
-            lis: []
+            lis: [
+                {
+                    index: "A",
+                    list: [{ key: 0, value: "" }]
+                }
+            ],
+            indexs: [],
+            navOffsetX: 0,
+            moving: false
         };
     },
     computed: {
-        barList() {
-            return this.lis.filter(e => {
-                return e.key % 10 == 0;
-            });
-        }
+        // barList() {
+        //     return this.lis.filter(e => {
+        //         return e.key % 10 == 0;
+        //     });
+        // }
     },
     mounted() {
         let length = 0;
@@ -47,14 +74,31 @@ export default {
             // freeScroll: true,
             // eventPassthrough: "vertical"
         });
-        while (length <= 100) {
-            length++;
-            this.lis.push({
-                key: length,
-                value: length
+        let allChart = getAllChars();
+        let lis = [];
+        let indexs = [];
+        while (allChart.length) {
+            let index = allChart.shift();
+            let num = Math.round(Math.random() * 20);
+            let item = {
+                list: [],
+                index
+            };
+            for (let i = 0; i < num; i++) {
+                item.list.push({
+                    key: i,
+                    value: `${index}-${i}`
+                });
+            }
+            lis.push(item);
+            indexs.push({
+                index,
+                key: index,
+                hidden: true
             });
         }
-
+        this.lis = lis;
+        this.indexs = indexs;
         this.$nextTick(() => {
             console.log(111);
             // this.scroll.refresh();
@@ -91,18 +135,103 @@ export default {
             alert(1);
         },
         scrollto(item) {
-            console.log({ item, target: this.$refs });
-            this.scroll.scrollToElement(this.$refs[`target_${item.key}`][0]);
+            // console.log({ item, target: this.$refs });
+            // this.scroll.scrollToElement(this.$refs[`target_${item.key}`][0]);
         },
         tap(ev) {
-            console.log("tap", arguments);
-            let target = ev.target || ev.srcElement;
-            if (target.dataset.key) {
-                this.scroll.scrollToElement(
-                    this.$refs[`target_${target.dataset.key}`][0]
-                );
-            }
+            // console.log("tap", arguments);
+            // let target = ev.target || ev.srcElement;
+            // if (target.dataset.key) {
+            //     this.scroll.scrollToElement(
+            //         this.$refs[`target_${target.dataset.key}`][0]
+            //     );
+            // }
             // console.log({ item, target: this.$refs });
+        },
+        indexStart(e) {
+            // 如果不是从索引栏开始滑动，则直接return
+            // 保证了左侧内容区域能够正常滑动
+            console.log({ e });
+            if (e.target.tagName !== "LI") {
+                return;
+            }
+
+            // 记录开始的clientX值，这个clientX值将在之后的滑动中持续用到，用于定位
+            this.navOffsetX = e.changedTouches[0].clientX;
+
+            // 内容滑动到指定区域
+            this.scrollList(e.changedTouches[0].clientY);
+            // if (indicatorTime) {
+            //     clearTimeout(indicatorTime);
+            // }
+            this.moving = true;
+
+            // 在window区域注册滑动和结束事件
+            window.addEventListener("touchmove", this.handleTouchMove, {
+                passive: false
+            });
+            window.addEventListener("touchend", this.handleTouchEnd);
+        },
+        scrollList(y) {
+            // 通过当前的y值以及之前记录的clientX值来获得索引栏中的对应item
+            var currentItem = document.elementFromPoint(this.navOffsetX, y);
+            if (
+                !currentItem ||
+                !currentItem.classList.contains("indexlist-navitem")
+            ) {
+                return;
+            }
+            let { key } = currentItem.dataset;
+            if (!key) return;
+            console.log({ currentItem });
+            // 显示指示器
+            // currentIndicator = currentItem.innerText;
+            // indicator.innerText = currentIndicator;
+            // indicator.style.display = "";
+            let targes = this.lis.find(v => v.index == key);
+            let index = 0;
+            this.indexs.forEach(element => {
+                if (element.index == key) index = element;
+                element.hidden = true;
+            });
+            index.hidden = false;
+            setTimeout(() => {
+                index.hidden = true;
+            }, 500);
+            if (targes) {
+                this.scroll.scrollToElement(this.$refs[`target_${key}`][0]);
+                console.log(index, targes);
+            }
+            // 找到左侧内容的对应section
+            // var targets = [].slice.call(sections).filter(function(section) {
+            //     var index = section.getAttribute("data-index");
+            //     return index === currentItem.innerText;
+            // });
+            // var targetDOM;
+            // if (targets.length > 0) {
+            //     targetDOM = targets[0];
+            //     // 通过对比要滑动到的区域的top值与最开始的一个区域的top值
+            //     // 两者的差值即为要滚动的距离
+            //     content.scrollTop =
+            //         targetDOM.getBoundingClientRect().top -
+            //         firstSection.getBoundingClientRect().top;
+
+            // 或者使用scrollIntoView来达到相同的目的
+            // 不过存在兼容性的问题
+            // targetDOM.scrollIntoView();
+            // }
+        },
+        handleTouchMove(e) {
+            e.preventDefault();
+            console.log("handleTouchMove", { e });
+            this.scrollList(e.changedTouches[0].clientY);
+        },
+        handleTouchEnd() {
+            this.moving = false;
+            window.removeEventListener("touchmove", this.handleTouchMove, {
+                passive: false
+            });
+            window.removeEventListener("touchend", this.handleTouchEnd);
         }
     }
 };
@@ -115,12 +244,24 @@ header {
     pointer-events: none;
     /* 禁止鼠标事件 */
 }
+li ul {
+    list-style: none;
+}
+* {
+    padding: 0px;
+    margin: 0;
+}
 html,
 body {
     height: 100%;
 }
 .container {
-    height: 300px;
+    position: absolute;
+    left: 0px;
+    right: 100px;
+    bottom: 50px;
+    top: 30px;
+    /* width: 100px; */
     border: 1px solid;
     overflow: hidden;
 }
@@ -145,7 +286,29 @@ body {
 .bar li {
     width: 18px;
     height: 18px;
+    margin: 5px 0;
     border-radius: 50%;
+    /* background: saddlebrown; */
+    text-align: center;
+    vertical-align: middle;
+    font-size: 12px;
+    list-style: none;
+}
+.indexlist-navitem {
+    position: relative;
+}
+.indexlist-title {
+    position: absolute;
+    left: -20px;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
     background: saddlebrown;
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: middle;
+}
+.hidden {
+    display: none;
 }
 </style>
