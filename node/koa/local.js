6bf28ed3acadd1533125cefc354cae6361f3b746
caoshiyuan <codesircao@126.com>
2020-09-27 13:16:47 +0800

    add:不知道是啥

---
+++ node/koa/local.js
@@ -0,0 +1,13 @@
+let mysql_conf = {
+    dbname: 'meiyingying',
+    uname: 'yktour_bpm',
+    upwd: 'n58#^VU*%uv2Vx3N',
+    host: 'rm-2ze83oz02tj45050xco.mysql.rds.aliyuncs.com',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+}
